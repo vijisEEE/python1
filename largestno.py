bfc455fd18d3058e5b252582637cbaec7c1139ad
vijisEEE <36723491+vijisEEE@users.noreply.github.com>
2018-02-23 15:33:02 +0530

    Create largestno.py

---
+++ largestno.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first num:"))
+b=int(input("Enter the second num:"))
+c=int(input("Enter the third num:"))
+if(a>b)and(a>c):
+  print(a," is the greast number")
+elif(b>c):
+  print(b," is the greast number")
+else:
+  print(c,"is the greast number")
